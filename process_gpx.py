3c97e77c3f6efc6fb4f6d0d6df9f1f597a816b33
Joe Germuska <joe@germuska.com>
2020-05-19 09:20:18 -0500

    update with LFL labels and some new data

--- process_gpx.py
+++ process_gpx.py
@@ -1,3 +1,6 @@
+# TODO: animate the paths...
+# https://docs.mapbox.com/mapbox-gl-js/example/live-update-feature/
+
 from lxml.etree import parse,dump
 from pathlib import Path
 from osgeo import ogr
@@ -28,20 +31,18 @@ def style_function(feature):
         'color': 'red' if feature['properties']['type'] == 'Cycling' else 'blue'
     }
 
-if __name__ == '__main__':
-    p = Path('export') # parameterize input dir?
-    walks = list(p.glob('*.gpx'))
-    routes = [parse_gpx(walk) for walk in walks]
+def gpx_to_geojson(path, output_file):
+    routes = [parse_gpx(walk) for walk in path.glob('*.gpx')]
     df = gpd.GeoDataFrame(routes)
     df.sort_values('time',inplace=True)
 
     home = [42.045465,-87.7161625]
-    m = folium.Map(
-        location=[42.03,-87.7161625],#home,
-        tiles='OpenStreetMap',
-        zoom_start=15,
-        height=2500
-    )
+    # m = folium.Map(
+    #     location=[42.03,-87.7161625],#home,
+    #     tiles='OpenStreetMap',
+    #     zoom_start=15,
+    #     height=2500
+    # )
 
     j = json.loads(df['geometry'].to_json())
     for i,feature in enumerate(j['features']):
@@ -51,6 +52,42 @@ if __name__ == '__main__':
         feature['properties']['date'] = row['time'].strftime("%Y-%m-%d")
 
     j['features'].sort(key=lambda x: x['properties']['date'])
-    with open("paths.geojson","w") as f:
+    with open(output_file,"w") as f:
         json.dump(j,f)
-        
+
+def fixup_markers(fn):
+
+    import re
+    ICONS = [
+        (re.compile('^.*pantry.*$',re.IGNORECASE), 'grocery-15')
+    ]
+
+    DROP = [ # old stuff that is in saved places but shouldn't be on the map
+        'Home',
+        'Work',
+        "Sandy & Sarah's",
+        'Tom and Megan Germuska',
+        'The fireworks spot'
+    ]
+    j = json.load(open(fn))
+    to_remove = []
+    for place in j['features']:
+        if place['properties']['name'] in DROP:
+            to_remove.append(place)
+        else:
+            place['properties']['icon'] = 'library-15' # default
+            # my maps export from Google has... coordinates reversed?
+            # geojson wants [ lng, lat ] -- since we're around here, we can assume negative is a lng
+            if place['geometry']['coordinates'][1] < 0:
+                place['geometry']['coordinates'] = list(reversed(place['geometry']['coordinates']))
+            for pat,icon in ICONS:
+                if pat.match(place['properties']['name']):
+                    place['properties']['icon'] = icon
+    for place in to_remove:
+        j['features'].remove(place)
+    json.dump(j,open(fn,'w'),indent=2)
+
+if __name__ == '__main__':
+    p = Path('export') 
+    gpx_to_geojson(p, "paths.geojson")
+    fixup_markers('lfl.geojson')
