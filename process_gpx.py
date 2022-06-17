a3db981f67c28ce75f430bf247f1a134c0a1229c
Joe Germuska <joe@germuska.com>
2022-06-17 10:36:55 -0500

    update for 6/17/22 and process change to support manual overrides

--- process_gpx.py
+++ process_gpx.py
@@ -5,12 +5,12 @@ from lxml.etree import parse,dump
 from pathlib import Path
 from osgeo import ogr
 import datetime
+from dateutil.parser import isoparse
 import geopandas as gpd
-import folium
-from shapely.geometry import Polygon, LineString
+from shapely.geometry import LineString, mapping, shape
 import json
 
-FIXES = {
+NAME_FIXES = {
     # user errors: forgot to switch before starting
     'Cycling 5/26/20 8:28 am': lambda name: name.replace('Cycling', 'Walking'),
     'Walking 7/19/20 10:49 am': lambda name: name.replace('Walking', 'Cycling'),
@@ -19,25 +19,47 @@ FIXES = {
 def parse_gpx(p):
     doc = parse(p.open())
     name = doc.xpath('gpx:trk/gpx:name',namespaces={'gpx': 'http://www.topografix.com/GPX/1/1'})[0].text
-    if name in FIXES:
-        name = FIXES[name](name)
+    if name in NAME_FIXES:
+        name = NAME_FIXES[name](name)
     time = doc.xpath('gpx:trk/gpx:time',namespaces={'gpx': 'http://www.topografix.com/GPX/1/1'})[0].text
     route = [(float(trkpt.attrib['lon']),float(trkpt.attrib['lat'])) 
              for trkpt in doc.xpath('//gpx:trkpt',namespaces={'gpx': 'http://www.topografix.com/GPX/1/1'})]
     d = {
         'name': name,
-        'time': datetime.datetime.strptime(time, "%Y-%m-%dT%H:%M:%SZ"),
+        'time': isoparse(time),
         'time_str': time,
         'geometry': LineString(route)
     }
     
     return d
 
+
+def one_gpx_to_json_file(gpx_path):
+    """Create a single JSON file structurally compatible with the components
+    passed in to the dataframe. The name will be the same as the input, with 
+    .gpx changed to .json. Note this file is json, not geojson, but has a 'geometry'
+    key which has a value which is a geojson LineString.
+    """
+    parsed = parse_gpx(gpx_path)
+    parsed['geometry'] = mapping(parsed['geometry'])
+    del parsed['time'] # not serializable, but we have time_str
+    json_path = gpx_path.parent / f"{gpx_path.stem}.json"
+    json.dump(parsed,json_path.open('w'))
+
 def gpx_to_geojson(path, output_file):
     routes = []
     for walk in path.glob('*.gpx'):
         try:
-            routes.append(parse_gpx(walk))
+            fixed = walk.parent / f'{walk.stem}.json'
+            if fixed.exists():
+                print(f"reading override {fixed}")
+                parsed = json.load(fixed.open())
+                # must manage time and shapely serialization/deserialization specially
+                parsed['geometry'] = shape(parsed['geometry'])
+                parsed['time'] = isoparse(parsed['time_str'])
+            else:
+                parsed = parse_gpx(walk)
+            routes.append(parsed)
         except Exception as e:
             print(f"Error processing {walk}")
             print(f"{e}")
